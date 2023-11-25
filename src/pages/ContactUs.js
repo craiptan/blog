bb22ef5fc983e0dddc97358faeff02d386091553
craiptan <spunkychris17@gmail.com>
2023-11-25 10:27:57 +0300

    starterd contact us page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,15 +1,15 @@
 import Head from "next/head";
-import Navbar from "@/components/Navbar";
-import SectionTitle from "@/components/SectionTitle";
-import CompanyHero from "@/components/companyHero" 
+import Navbar from "@/components/Navbar"; 
 import Footer from "@/components/footer";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
+import { styled } from '@mui/system';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
 
 export default function ContactUs(){
   const handleSubmit = (event) => {
@@ -20,6 +20,59 @@ export default function ContactUs(){
       password: data.get('password'),
     });
   };
+
+  const blue = {
+    100: '#DAECFF',
+    200: '#b6daff',
+    400: '#3399FF',
+    500: '#007FFF',
+    600: '#0072E5',
+    900: '#003A75',
+  };
+
+  const grey = {
+    50: '#F3F6F9',
+    100: '#E5EAF2',
+    200: '#DAE2ED',
+    300: '#C7D0DD',
+    400: '#B0B8C4',
+    500: '#9DA8B7',
+    600: '#6B7A90',
+    700: '#434D5B',
+    800: '#303740',
+    900: '#1C2025',
+  };
+
+  const Textarea = styled(BaseTextareaAutosize)(
+    ({ theme }) => `
+    width: 100%;
+    font-family: IBM Plex Sans, sans-serif;
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: 12px;
+    border-radius: 12px 12px 0 12px;
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+
+    &:hover {
+      border-color: ${blue[400]};
+    }
+
+    &:focus {
+      outline: 0;
+      border-color: ${blue[400]};
+      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
+    }
+
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `,
+  );
     return(
     <>
         <Head>
@@ -30,8 +83,8 @@ export default function ContactUs(){
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div class="container mx-auto px-4">
-          <Navbar />
+        <div className="container mx-auto px-4">
+          <Navbar />   
           <Box
           sx={{
             marginTop: 8,
@@ -44,26 +97,21 @@ export default function ContactUs(){
             Get in touch
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+            <Grid container item xs={6} spacing={2}>
+              <Grid item xs={12}>
                 <TextField
                   autoComplete="given-name"
-                  name="firstName"
+                  name="fullName"
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
+                  id="fullName"
+                  label="Full Name"
                   autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
+                  InputProps={{
+                    style: {
+                      borderRadius: "12px",
+                    }
+                  }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -74,33 +122,41 @@ export default function ContactUs(){
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  InputProps={{
+                    style: {
+                      borderRadius: "12px",
+                    }
+                  }}
+                  
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
+                  id="subject"
+                  label="Subject"
+                  name="subject"
+                  autoComplete="subject"
+                  InputProps={{
+                    style: {
+                      borderRadius: "12px",
+                    }
+                  }}
+                  
                 />
               </Grid>
               <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
+                <Textarea aria-label="empty textarea" minRows={3}  placeholder="Empty" />
               </Grid>
             </Grid>
             <Button
               type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              variant="outlined"
+              color="success"
+              endIcon={<SendIcon />}
             >
-              Sign Up
+              Send
             </Button>
             
           </Box>
