a6a5ef30de451b0a9f395288181475cd74199dfd
Christopher Githui <47109296+craiptan@users.noreply.github.com>
2024-11-16 11:42:03 +0300

    Update send_mail.js

--- src/pages/api/send_mail.js
+++ src/pages/api/send_mail.js
@@ -5,38 +5,38 @@ import nodemailer from "nodemailer"
     try {
     console.log(req.body.subject);
 
-    const OAuth2 = google.auth.OAuth2;
-    const OAuth2Client = new OAuth2(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        'https://developers.google.com/oauthplayground',
-    );
+    // const OAuth2 = google.auth.OAuth2;
+    // const OAuth2Client = new OAuth2(
+    //     process.env.CLIENT_ID,
+    //     process.env.CLIENT_SECRET,
+    //     'https://developers.google.com/oauthplayground',
+    // );
 
-    OAuth2Client.setCredentials({
-        refresh_token: process.env.REFRESH_TOKEN
-    });;
+    // OAuth2Client.setCredentials({
+    //     refresh_token: process.env.REFRESH_TOKEN
+    // });;
 
-    const tokens = await OAuth2Client.refreshAccessToken();
-    const accessToken = tokens.credentials.access_token;
+    // const tokens = await OAuth2Client.refreshAccessToken();
+    // const accessToken = tokens.credentials.access_token;
 
-    const transport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.EMAIL,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: accessToken,
-        }
-    });
+    // const transport = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         type: 'OAuth2',
+    //         user: process.env.EMAIL,
+    //         clientId: process.env.CLIENT_ID,
+    //         clientSecret: process.env.CLIENT_SECRET,
+    //         refreshToken: process.env.REFRESH_TOKEN,
+    //         accessToken: accessToken,
+    //     }
+    // });
 
-    const mailRes = await transport.sendMail({
-        from: req.body.email,
-        to: 'whiptixtechnologies@gmail.com',
-        subject: req.body.subject,
-        text: 'Name: '+ req.body.fullname+ ' Email: '+req.body.email+' Message: '+ req.body.message,
-    });
+    // const mailRes = await transport.sendMail({
+    //     from: req.body.email,
+    //     to: 'whiptixtechnologies@gmail.com',
+    //     subject: req.body.subject,
+    //     text: 'Name: '+ req.body.fullname+ ' Email: '+req.body.email+' Message: '+ req.body.message,
+    // });
 
     console.log(mailRes);
 }catch(error){
