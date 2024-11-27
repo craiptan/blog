fc9f17967770e310867468666eb391b66e7e35b2
craiptan <>
2024-11-27 08:57:13 +0300

    removed contact us on hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,13 +12,13 @@ const Hero = () => {
               Actualizing digital concepts through creative technology solutions.
             </h1>
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <a
+              {/* <a
                 href="/ContactUs"
                 target="_self"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-blue-600 rounded-md ">
                 GET IN TOUCH
-              </a>
+              </a> */}
              
             </div>
           </div>
